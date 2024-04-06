c49b856a1d23791d20e41b83b503fcef1c517760
Ahmed-Mohamed-Hassen-Mohamed <93393919+Ahmed-Mohamed-Hassen-Mohamed@users.noreply.github.com>
2024-04-06 22:23:34 +0200

    Update dashboard.component.ts

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,8 +1,5 @@
-import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { AddGroupComponent } from '../add-group/add-group.component';
-import { JoinGroupComponent } from '../join-group/join-group.component';
-import { UserService } from 'src/app/services/user.service';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,24 +7,5 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-  constructor(private dailog: MatDialog, private userService: UserService) {}
-  ngOnInit(): void {
-    this.getProfile();
-  }
-  addGroup() {
-    let dailog = this.dailog.open(AddGroupComponent);
-    dailog.afterClosed().subscribe((result) => {});
-  }
-  joinGroup() {
-    let dailog = this.dailog.open(JoinGroupComponent);
-    dailog.afterClosed().subscribe((result) => {});
-  }
-  user: any;
-  getProfile() {
-    this.userService.getProfile().subscribe({
-      next: (user: any) => {
-        this.user = user;
-      },
-    });
-  }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 }
